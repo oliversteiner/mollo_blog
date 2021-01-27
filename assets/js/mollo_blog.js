d855027e9b6e1793401db94edc7f89c80619c84a
Oliver Steiner <oliver@mollo.ch>
2021-01-27 16:21:20 +0100

    remove debug code

--- assets/js/mollo_blog.js
+++ assets/js/mollo_blog.js
@@ -1,11 +1,47 @@
-(function($, Drupal, drupalSettings) {
+(function ($, Drupal, drupalSettings) {
   Drupal.behaviors.molloBlog = {
     attach(context, settings) {
-      console.log('Mollo Blog');
 
-      $('#mollo-blog', context)
-        .once('mollo-blog')
-        .each(() => {});
+      if (!drupalSettings.molloBlog) {
+        drupalSettings.molloBlog = {
+          once: false,
+          targets: [],
+          activeTargetID: 0,
+        }
+      }
+
+      if (!drupalSettings.molloBlog.once) {
+        drupalSettings.molloBlog.once = true;
+        this.load();
+      }
     },
+
+    load() {
+      $(document).ready(function () {
+
+        const $targets = $('.blog-teaser-detail');
+        drupalSettings.molloBlog.targets = $targets;
+        $($targets[0]).show();
+
+
+      })
+
+    },
+
+    showDetail(targetID) {
+      if (targetID !== drupalSettings.molloBlog.activeTargetID) {
+        const $targets = drupalSettings.molloBlog.targets
+        $targets.hide();
+        $('.blog-teaser-detail-' + targetID).show();
+        drupalSettings.molloBlog.activeTargetID = targetID
+
+      }
+    },
+    goToNode(targetID) {
+
+    }
   };
 })(jQuery, Drupal, drupalSettings);
+
+
+
